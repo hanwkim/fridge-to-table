15f812becbbd728720a0db67cbd6054659d25a88
Han Kim <hanwkim88@gmail.com>
2023-01-19 17:58:48 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import Header from './components/Header/Header';
+import Header from "./components/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Header />
+      <Routes>
+        {/* <Route to='/' element={<HomePage />} /> */}
+        {/* <Route to='/recipes' element={<RecipeListPage />} />
+        <Route to='/recipes/:id' element={<RecipePage />} /> */}
+      </Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
