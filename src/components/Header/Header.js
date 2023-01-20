5ff9e77bf0e1af0951857ee19aba5fcce1a4c5d8
Han Kim <hanwkim88@gmail.com>
2023-01-19 21:33:52 -0500

    Add search functionality.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import './Header.scss';
 
 export default function Header() {
+	const navigate = useNavigate();
+
+	function submitHandler(event) {
+		event.preventDefault();
+		navigate(`/recipes?i=${event.target.search.value}`)
+	}
+
+
 	return (
 		<header className="header">
 			<h1 className="header__title">Fridge to Table</h1>
 			<nav className="nav">
-				<form className="nav__form">
+				<form onSubmit={submitHandler} className="nav__form">
 					<input
 						type="search"
 						name="search"
--- src/pages/RecipeListPage/RecipeListPage.js
+++ src/pages/RecipeListPage/RecipeListPage.js
@@ -2,29 +2,33 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './RecipeListPage.scss';
 import RecipeCard from '../../components/RecipeCard/RecipeCard';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 export default function RecipeListPage() {
     const [recipeList, setRecipeList] = useState(null);
-    const BASE_API_URL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast";
+    const [searchParams] = useSearchParams();
+
+    let urlQuery = searchParams.get('i');
+
+    const BASE_API_URL = "https://www.themealdb.com/api/json/v1/1/filter.php";
 
     useEffect(() => {
         try {
             async function getRecipeList() {
-                const { data } = await axios.get(BASE_API_URL);
+                const { data } = await axios.get(`${BASE_API_URL}?i=${urlQuery}`);
                 setRecipeList(data.meals);
             }
             getRecipeList();
         } catch(error) {
             console.log(error);
         }
-    }, [])
+    }, [urlQuery])
 
     return (
         <section className="list-page">
             {recipeList && recipeList.map((recipe) => {
                 return (
-                <Link to={`/recipes/${recipe.idMeal}`} >
+                <Link key={recipe.idMeal} to={`/recipes/${recipe.idMeal}`} >
                     <RecipeCard recipe={recipe} />
                 </Link>)
             })}
